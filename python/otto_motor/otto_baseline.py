a46289dbfeb1a7678af95e1981e45f265a5e0028
valentin.boettcher@mailbox.tu-dresden.de <hiro@protagon.space>
2022-12-01 18:56:26 -0500

    add basic simulation

--- python/otto_motor/otto_baseline.py
+++ python/otto_motor/otto_baseline.py
@@ -8,45 +8,60 @@ import qutip as qt
 import utilities as ut
 import stocproc
 import matplotlib.pyplot as plt
+import otto_utilities as ot
 
 import ray
 ray.shutdown()
-#ray.init(address='auto')
-ray.init()
+ray.init(address='auto')
+#ray.init()
 
 from hops.util.logging_setup import logging_setup
 import logging
 logging_setup(logging.INFO)
-plt.rcParams['figure.figsize'] = (16,8)
+plt.rcParams['figure.figsize'] = (12,4)
 
 model = OttoEngine(
-    δ=[.4, .4],
+    δ=[0.4, 0.4],
     ω_c=[1, 1],
     ψ_0=qt.basis([2], [1]),
-    description=f"An otto cycle on the way to finding the baseline.",
+    description=f"A basic near-markovian, weakly coupled Otto Cycle that actually works.",
     k_max=4,
-    bcf_terms=[6]*2,
+    bcf_terms=[6] * 2,
     truncation_scheme="simplex",
-    driving_process_tolerances=[StocProcTolerances(1e-3, 1e-3)] * 2,
-    thermal_process_tolerances=[StocProcTolerances(1e-3, 1e-3)] * 2,
-    T = [1, 10],
+    driving_process_tolerances=[StocProcTolerances(1e-4, 1e-4)] * 2,
+    thermal_process_tolerances=[StocProcTolerances(1e-4, 1e-4)] * 2,
+    T=[1, 10],
     therm_methods=["tanhsinh", "fft"],
     Δ=1,
     num_cycles=5,
-    Θ=1/.05,
-    dt=.001,
-    timings_H=(0, .1, .5, .6),
-    timings_L=((.6, .7, .9, 1), (.1, .2, .4, .5)),
+    Θ=1.5 / 0.05,
+    dt=0.001,
+    timings_H=(0, 0.1, 0.5, 0.6),
+    timings_L=((0.6, 0.7, 0.9, 1), (0.1, 0.2, 0.4, 0.5)),
     streaming_mode=True,
 )
 
-plt.plot(model.t, model.H.operator_norm(model.t) - 1, label="H")
-plt.plot(model.t, model.coupling_operators[0].operator_norm(model.t) * 2, label="cold")
-plt.plot(model.t, model.coupling_operators[1].operator_norm(model.t) * 2, label="hot")
-plt.legend()
+# model = OttoEngine(
+#     δ=[0.4, 0.4],
+#     ω_c=[1, 1],
+#     ψ_0=qt.basis([2], [1]),
+#     description=f"An otto cycle on the way to finding the baseline.",
+#     k_max=3,
+#     bcf_terms=[4] * 2,
+#     truncation_scheme="simplex",
+#     driving_process_tolerances=[StocProcTolerances(1e-3, 1e-3)] * 2,
+#     thermal_process_tolerances=[StocProcTolerances(1e-3, 1e-3)] * 2,
+#     T=[1, 10],
+#     therm_methods=["tanhsinh", "fft"],
+#     Δ=1,
+#     num_cycles=4,
+#     Θ=1.5 / 0.05,
+#     dt=0.001,
+#     timings_H=(0, 0.1, 0.3, 0.4),
+#     timings_L=((0.4, 0.5, 0.9, 1), (0.1, 0.11, 0.29, 0.3)),
+#     streaming_mode=True,
+# )
 
-ω = np.linspace(.01, 3, 1000)
-plt.plot(ω, model.full_thermal_spectral_density(0)(ω) * model.bcf_scales[0])
-plt.plot(1, model.full_thermal_spectral_density(0)(1) * model.bcf_scales[0], marker="o")
-plt.plot(ω, model.full_thermal_spectral_density(1)(ω) * model.bcf_scales[1])
-plt.plot(2, model.full_thermal_spectral_density(1)(2) * model.bcf_scales[1], marker="o")
+ot.plot_cycle(model)
+
+ot.plot_sd_overview(model)
