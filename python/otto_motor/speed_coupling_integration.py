326eba32120f079818298f65f763ee35aa806df5
valentin.boettcher@mailbox.tu-dresden.de <hiro@protagon.space>
2022-12-16 19:59:47 -0500

    use increment for intergration

--- python/otto_motor/speed_coupling_integration.py
+++ python/otto_motor/speed_coupling_integration.py
@@ -1,3 +1,3 @@
 from speed_coupling_scan import *
 
-ot.integrate_online_multi(models, 10_000, increment=10, analyze_kwargs=dict(every=100))
+ot.integrate_online_multi(models, 10_000, increment=1000, analyze_kwargs=dict(every=100))
--- python/otto_motor/timing_scan_integration.py
+++ python/otto_motor/timing_scan_integration.py
@@ -1,5 +1,3 @@
 from timing_scan import *
 
-ot.integrate_online_multi(models, 1000, analyze_kwargs=dict(every=100))
-ot.integrate_online_multi(models, 2000, analyze_kwargs=dict(every=100))
-ot.integrate_online_multi(models, 4000, analyze_kwargs=dict(every=100))
+ot.integrate_online_multi(models, 10_000, increment=2000, analyze_kwargs=dict(every=100))
