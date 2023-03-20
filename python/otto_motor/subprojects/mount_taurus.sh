428258311c0707a0bc904b43ea577d87c2aa12b1
valentin.boettcher@mailbox.tu-dresden.de <hiro@protagon.space>
2023-03-20 11:00:14 -0400

    add taurus convenience script

---
+++ python/otto_motor/subprojects/mount_taurus.sh
@@ -0,0 +1,1 @@
+sshfs -oIdentityFile=~/.ssh/id_ed25519_taurus s8896854@taurusexport.hrsk.tu-dresden.de:/beegfs/ws/0/s8896854-ot_cpl/project/python/otto_motor/subprojects/cycle_length_coupling_strength taurus
