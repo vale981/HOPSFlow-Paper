9ac1d06911783d2588b859876954f7adfb325cdd
valentin.boettcher@mailbox.tu-dresden.de <hiro@protagon.space>
2023-04-03 11:58:47 -0400

    mount taurus script still broken

--- python/otto_motor/subprojects/mount_taurus.sh
+++ python/otto_motor/subprojects/mount_taurus.sh
@@ -1,1 +1,1 @@
-sshfs -oIdentityFile=~/.ssh/id_ed25519_taurus s8896854@taurusexport.hrsk.tu-dresden.de:/beegfs/ws/0/s8896854-ot_cpl/project/python/otto_motor/subprojects/cycle_length_coupling_strength taurus
+sshfs -oIdentityFile=~/.ssh/id_ed25519_taurus s8896854@taurusexport.hrsk.tu-dresden.de:/beegfs/ws/0/s8896854-ot_cpl/project/python/otto_motor/subprojects/cycle_shift taurus
